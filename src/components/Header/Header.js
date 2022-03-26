3640986e1e6afc7d44dc8a69a9f67b7077820c14
pushpitanandy <nandypushpita@gmail.com>
2022-03-26 12:45:05 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Chocolate World</h1>
+            <p>Choose any four items.</p>
+        </div>
+    );
+};
+
+export default Header;
